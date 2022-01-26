6dbddb7ed6e912a785236067a286d9f6ff969a2f
mouradIdo <mouradanas2007@gmail.com>
2022-01-26 19:52:01 +0100

    second

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print ("PyChm")
+print("PyChm")
+
+print("second commit")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
